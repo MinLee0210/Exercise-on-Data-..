ea0fe73b41b145bd1d58ccf8520664194312c07e
minhleduc0210 <bastreet124@gmail.com>
2022-07-04 12:31:56 +0700

    Learning via an example on Kaggle

--- exercise_2.py
+++ exercise_2.py
@@ -1,3 +1,6 @@
+# SOURCE: https://www.kaggle.com/code/lakhankumawat/drugs-classification-notebook-for-beginners/notebook
+
+from statistics import mode
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -45,13 +48,98 @@ num_cols = df.drop(cat_cols, axis=1).columns
 # WE REMOVE BY FINDING ITS FIRST AND THIRD QUANTILE TO FIND OUT ITS LOWER- AND UPPER- BOUND.
 # THEN WE REMOVE ALL THE DATAS THAT HIGHER THAN OR LOWER THEN THE UPPERBOUND AND THE LOWERBOUND RESPECTIVELY.
 
-Q1 = df['Na_to_K'].quantile(0.25)
-Q3 = df['Na_to_K'].quantile(0.75)
-IQR = Q3 - Q1
-upper = Q3 + 1.5*IQR
-lower = Q1 = 1.5*IQR
+# Q1 = df['Na_to_K'].quantile(0.25)
+# Q3 = df['Na_to_K'].quantile(0.75)
+# IQR = Q3 - Q1
+# upper = Q3 + 1.5*IQR
+# lower = Q1 = 1.5*IQR
 
-df = df[(df['Na_to_K'] < upper) & (df['Na_to_K'] > lower)]
+# df = df[(df['Na_to_K'] < upper) & (df['Na_to_K'] > lower)]
+# print(df.head(10))
 
-sns.boxplot(df['Na_to_K'])
-plt.show()
+# sns.boxplot(df['Na_to_K'])
+# plt.show()
+
+# CONVERT CATEGORICAL COLUMNS INTO NUMBERS USING LABEL ENCODING
+for column in cat_cols:
+    le = LabelEncoder()
+    df[column] = le.fit_transform(df[column])
+    
+# print(df.head(10))
+
+X_train, X_test, y_train, y_test = train_test_split(df.drop('Drug', axis=1), df['Drug'], test_size=0.2, 
+                                                    stratify=df['Drug'])
+
+# WE USE DIFFENT MODEL FOR PREDICTION. (NORMALLY, WE USE 3 MODELS.)
+
+# FIRST, WE USE LOGISTIC REGRESSION
+model_lr = LogisticRegression(solver='liblinear')
+model_lr.fit(X_train, y_train)
+ypred_lr = model_lr.predict(X_test)
+# print(confusion_matrix(y_test, ypred_lr))
+# print(classification_report(y_test, ypred_lr))
+
+# sns.heatmap(confusion_matrix(y_test, ypred_lr), annot=True)
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
+
+# WE USE DECISION TREE CLASSIFIER   
+model_dt = DecisionTreeClassifier()
+model_dt.fit(X_train, y_train)
+ypred_dt = model_dt.predict(X_test)
+# print(confusion_matrix(y_test, ypred_dt))
+# print(classification_report(y_test,ypred_dt))
+
+# sns.heatmap(confusion_matrix(y_test,ypred_dt),annot=True)
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
+
+# WE USE RANDOM FOREST CLASSIFIER
+model_rf=RandomForestClassifier()
+model_rf.fit(X_train, y_train)
+ypred_rf = model_rf.predict(X_test)
+# print(confusion_matrix(y_test, ypred_rf))
+# print(classification_report(y_test, ypred_rf))
+
+# sns.heatmap(confusion_matrix(y_test,ypred_rf),annot=True)
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
+
+# WE USE SUPPORT VECTOR MACHINE
+model_svc = SVC()
+model_svc.fit(X_train, y_train)
+ypred_svc = model_svc.predict(X_test)
+# print(confusion_matrix(y_test, ypred_svc))
+# print(classification_report(y_test, ypred_svc))
+
+# sns.heatmap(confusion_matrix(y_test,ypred_svc),annot=True)
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
+
+# K-NEIGHBORS
+model_KNN = KNeighborsClassifier()
+model_KNN.fit(X_train, y_train)
+ypred_KNN = model_KNN.predict(X_test)
+# print(confusion_matrix(y_test, ypred_KNN))
+# print(classification_report(y_test, ypred_KNN))
+
+# sns.heatmap(confusion_matrix(y_test,ypred_KNN),annot=True)
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
+
+# NAIVE-BAYES
+model_nb=MultinomialNB()
+model_nb.fit(X_train,y_train)
+ypred_nb=model_nb.predict(X_test)
+# print(confusion_matrix(y_test,ypred_nb))
+# print(classification_report(y_test, ypred_nb))
+
+# sns.heatmap(confusion_matrix(y_test, ypred_KNN), annot=True)
+# plt.xlabel('Predicted')
+# plt.ylabel('Actual')
+# plt.show()
